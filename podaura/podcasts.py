fc9ef87cc8d68a5c565f96d57c6a969d47467d91
Rutad2 <rakit2004@gmail.com>
2025-03-30 00:43:43 -0400

    Update podcasts.py

---
+++ podaura/podcasts.py
@@ -0,0 +1,18 @@
+podcast_recommendations = {
+    "happy": [
+        ("The Daily Smile", "https://www.stitcher.com/show/the-daily-smile"),
+        ("The Moth", "https://themoth.org/podcast")
+    ],
+    "sad": [
+        ("The Happiness Lab", "https://www.happinesslab.fm/"),
+        ("Unlocking Us", "https://brenebrown.com/podcast/introducing-unlocking-us/")
+    ],
+    "angry": [
+        ("Calm Pills", "https://calm-pills.com/"),
+        ("Meditative Story", "https://meditativestory.com/")
+    ],
+    "neutral": [
+        ("Stuff You Should Know", "https://www.iheart.com/podcast/105-stuff-you-should-know-26940277/"),
+        ("TED Talks Daily", "https://www.ted.com/podcasts/ted-talks-daily")
+    ]
+}
