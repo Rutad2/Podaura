7d98accbac2344cb62d4f913faaebb895f378067
btessera <btessera@gmu.edu>
2025-03-30 08:22:32 -0400

    Update podcasts.py

--- podaura/podcasts.py
+++ podaura/podcasts.py
@@ -1,18 +1,34 @@
 podcast_recommendations = {
+    "excited": [
+        ("The Tony Robbins Podcast", "https://www.tonyrobbins.com/podcast/"),
+        ("How I Built This", "https://www.npr.org/sections/how-i-built-this/")
+    ],
     "happy": [
         ("The Daily Smile", "https://www.stitcher.com/show/the-daily-smile"),
         ("The Moth", "https://themoth.org/podcast")
+    ],
+    "relaxed": [
+        ("Nothing Much Happens", "https://www.nothingmuchhappens.com/"),
+        ("Calm Pills", "https://calm-pills.com/")
+    ],
+    "neutral": [
+        ("Stuff You Should Know", "https://www.iheart.com/podcast/105-stuff-you-should-know-26940277/"),
+        ("TED Talks Daily", "https://www.ted.com/podcasts/ted-talks-daily")
+    ],
+    "melancholy": [
+        ("On Being", "https://onbeing.org/"),
+        ("This American Life", "https://www.thisamericanlife.org/")
     ],
     "sad": [
         ("The Happiness Lab", "https://www.happinesslab.fm/"),
         ("Unlocking Us", "https://brenebrown.com/podcast/introducing-unlocking-us/")
     ],
     "angry": [
-        ("Calm Pills", "https://calm-pills.com/"),
-        ("Meditative Story", "https://meditativestory.com/")
+        ("The Joe Rogan Experience", "https://open.spotify.com/show/4rOoJ6Egrf8K2IrywzwOMk"),
+        ("Pod Save America", "https://crooked.com/podcast-series/pod-save-america/")
     ],
-    "neutral": [
-        ("Stuff You Should Know", "https://www.iheart.com/podcast/105-stuff-you-should-know-26940277/"),
-        ("TED Talks Daily", "https://www.ted.com/podcasts/ted-talks-daily")
+    "stressed": [
+        ("The Calm Collective", "https://www.thecalmcollective.com/podcast"),
+        ("Sleepy", "https://www.sleepyradio.com/")
     ]
 }
