0fae873cf1ce7f37f873351c8fdcb0aca9c91085
btessera <btessera@gmu.edu>
2025-03-30 08:23:38 -0400

    Update podcasts.py

--- podaura/podcasts.py
+++ podaura/podcasts.py
@@ -1,34 +1,58 @@
 podcast_recommendations = {
     "excited": [
         ("The Tony Robbins Podcast", "https://www.tonyrobbins.com/podcast/"),
-        ("How I Built This", "https://www.npr.org/sections/how-i-built-this/")
+        ("How I Built This", "https://www.npr.org/sections/how-i-built-this/"),
+        ("TED Talks Daily", "https://www.ted.com/podcasts/ted-talks-daily"),
+        ("The Best One Yet", "https://tboypod.com/"),
+        ("SmartLess", "https://wondery.com/shows/smartless/")
     ],
     "happy": [
         ("The Daily Smile", "https://www.stitcher.com/show/the-daily-smile"),
-        ("The Moth", "https://themoth.org/podcast")
+        ("The Moth", "https://themoth.org/podcast"),
+        ("The Happiness Lab", "https://www.happinesslab.fm/"),
+        ("Good Life Project", "https://www.goodlifeproject.com/podcast/"),
+        ("Awesome with Alison", "https://podcasts.apple.com/us/podcast/awesome-with-alison/id1248644982")
     ],
     "relaxed": [
         ("Nothing Much Happens", "https://www.nothingmuchhappens.com/"),
-        ("Calm Pills", "https://calm-pills.com/")
+        ("Calm Pills", "https://calm-pills.com/"),
+        ("Sleepy", "https://www.sleepyradio.com/"),
+        ("Get Sleepy", "https://getsleepy.com/"),
+        ("The Slowdown", "https://www.slowdownshow.org/")
     ],
     "neutral": [
         ("Stuff You Should Know", "https://www.iheart.com/podcast/105-stuff-you-should-know-26940277/"),
-        ("TED Talks Daily", "https://www.ted.com/podcasts/ted-talks-daily")
+        ("TED Talks Daily", "https://www.ted.com/podcasts/ted-talks-daily"),
+        ("99% Invisible", "https://99percentinvisible.org/"),
+        ("The Daily", "https://www.nytimes.com/column/the-daily"),
+        ("Radiolab", "https://www.wnycstudios.org/podcasts/radiolab")
     ],
     "melancholy": [
         ("On Being", "https://onbeing.org/"),
-        ("This American Life", "https://www.thisamericanlife.org/")
+        ("This American Life", "https://www.thisamericanlife.org/"),
+        ("The Anthropocene Reviewed", "https://www.wnycstudios.org/podcasts/anthropocene-reviewed"),
+        ("Heavyweight", "https://gimletmedia.com/shows/heavyweight"),
+        ("Where Should We Begin? with Esther Perel", "https://www.estherperel.com/podcast")
     ],
     "sad": [
         ("The Happiness Lab", "https://www.happinesslab.fm/"),
-        ("Unlocking Us", "https://brenebrown.com/podcast/introducing-unlocking-us/")
+        ("Unlocking Us", "https://brenebrown.com/podcast/introducing-unlocking-us/"),
+        ("Terrible, Thanks for Asking", "https://www.ttfa.org/"),
+        ("The Mindful Minute", "https://merylarnett.com/the-mindful-minute"),
+        ("Griefcast", "https://play.acast.com/s/griefcast")
     ],
     "angry": [
         ("The Joe Rogan Experience", "https://open.spotify.com/show/4rOoJ6Egrf8K2IrywzwOMk"),
-        ("Pod Save America", "https://crooked.com/podcast-series/pod-save-america/")
+        ("Pod Save America", "https://crooked.com/podcast-series/pod-save-america/"),
+        ("The Argument", "https://www.nytimes.com/column/the-argument"),
+        ("You're Wrong About", "https://yourewrongabout.com/"),
+        ("Behind the Bastards", "https://www.iheart.com/podcast/105-behind-the-bastards-29236323/")
     ],
     "stressed": [
         ("The Calm Collective", "https://www.thecalmcollective.com/podcast"),
-        ("Sleepy", "https://www.sleepyradio.com/")
+        ("Sleepy", "https://www.sleepyradio.com/"),
+        ("Meditative Story", "https://meditativestory.com/"),
+        ("Mindful Muslim Podcast", "https://mindful-muslim.buzzsprout.com/"),
+        ("The Daily Meditation Podcast", "https://www.sipandom.com/podcast")
     ]
 }
